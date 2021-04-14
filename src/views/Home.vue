59dfd00d563bd1a7b035ccbe71f8cf29aeb3a6d6
Mitch McAffee <themcaffee@gmail.com>
2021-04-14 03:10:33 -0600

    nice

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Mitch McAffee</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </template>
 
