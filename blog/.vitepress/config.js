4c66348cdd1cc0a3d1a26425525b0dfdae31f23b
Mitch McAffee <themcaffee@gmail.com>
2022-08-29 13:55:05 -0700

    actual blog

---
+++ blog/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Mitch McAffee',
+  description: 'Thanks for all the fish',
+  themeConfig: {
+    siteTitle: 'Mitch McAffee',
+    nav: [
+        { text: 'About', link: '/about' }
+    ]
+  }
+}
---
+++ blog/about.md
@@ -0,0 +1,3 @@
+# About Me
+
+blah blah blah
