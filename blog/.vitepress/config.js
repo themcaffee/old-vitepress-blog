1d186e8ff098102576319a70ddc6301fb9be4542
Mitch McAffee <themcaffee@gmail.com>
2022-08-29 15:02:03 -0700

    Add contact info

--- blog/.vitepress/config.js
+++ blog/.vitepress/config.js
@@ -4,7 +4,10 @@ export default {
   themeConfig: {
     siteTitle: 'Mitch McAffee',
     nav: [
-        { text: 'About', link: '/about' }
+        { text: 'About', link: '/about' },
+        { text: 'Contact', link: '/contact' },
+        { text: 'Github', link: 'https://github.com/themcaffee' },
+        { text: 'LinkedIn', link: 'https://www.linkedin.com/in/mitch-mcaffee' }
     ]
   }
 }
---
+++ blog/contact.md
@@ -0,0 +1,3 @@
+# Let's talk!
+
+I am always open to exciting new opportunities, feel free to email me: contact@mitchmcaffee.com
